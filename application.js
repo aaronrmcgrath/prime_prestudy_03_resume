0dffe3ccfe31ab614f1586ea5af336d12699d9d3
Mbob023 <armcgrathmedia@gmail.com>
2016-02-24 08:06:30 -0600

    initial commit

---
+++ application.js
@@ -0,0 +1,10 @@
+ // $(document).ready(function() {
+ //   alert("Hello!");
+ // });
+
+ $(document).ready(function (){
+   $('.content').hide();
+   $('button').click(function() {
+    $('.content').toggle(0);
+   });
+ });
